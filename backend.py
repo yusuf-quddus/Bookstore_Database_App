d17459064b80b641fbd5b68256b34049bdb90c6b
Yusuf Quddus <yusuf_quddus@yahoo.com>
2020-08-07 21:19:29 -0700

    first commit backend
    connect, insert and view function

---
+++ backend.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+def connect():
+    conn=sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
+    conn.commit()
+    conn.close()
+
+def insert(title, author, year, isbn):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("INSERT INTO book VALUES (NULL, ?, ?, ?, ?)", (title, author, year, isbn))
+    conn.commit()
+    conn.close()
+
+def view(): 
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+
+
+connect()
+
+insert("The Sea", "John Tablet", 1918, 31214323)
+print(view())
